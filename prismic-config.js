dd1f6bd17eadc6e9fbf3f293ae89503b37276677
Paulina Gavilan <pauvpg@gmail.com>
2019-12-29 18:27:27 +0100

    default repo name

--- prismic-config.js
+++ prismic-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // -- Prismic repository GraphQL endpoint
-  apiEndpoint: "https://todo-app-sample.prismic.io/graphql",
+  apiEndpoint: "https://your-repo-name.prismic.io/graphql",
   // Replace the languages ​​with the corresponding ones in your Prismic repository
   defaultLanguage: "en-gb",
   langs: ["en-gb", "fr-fr"],
